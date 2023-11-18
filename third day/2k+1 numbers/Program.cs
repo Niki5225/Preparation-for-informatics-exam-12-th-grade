3a5cc6aaabb2bf92b2f219c3b0b7f414c734f0a8
Niki5225 <122698681+Niki5225@users.noreply.github.com>
2023-11-18 15:18:23 +0200

    thrid day

---
+++ third day/2k+1 numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Task
+{
+    public static void Main(string[] args)
+    {
+        int maxNumber = int.Parse(Console.ReadLine());
+        int currentNum = 1;
+
+        while (true)
+        {
+            Console.WriteLine(currentNum);
+            int possibleValue = 2 * currentNum + 1;
+            if (possibleValue <= maxNumber)
+            {
+                currentNum = possibleValue;
+            } else
+            {
+                break;
+            }
+        }
+    }
+}
