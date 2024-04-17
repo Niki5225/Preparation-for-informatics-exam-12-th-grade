c2bb04b3024e418dfce4df4974d867929717fc20
Niki5225 <122698681+Niki5225@users.noreply.github.com>
2024-04-17 23:17:01 +0300

    c# methods exercises

---
+++ methods/SmallestOfThreeNums.cs
@@ -0,0 +1,17 @@
+﻿namespace SmallestOfThreeNums
+{
+    class Task
+    {
+        static void Main()
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[3] { num1, num2, num3 };
+
+            Console.WriteLine(arr.Min());
+            
+        }
+    }
+}
---
+++ methods/VowelsCount.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace VowelsCount
+{
+    class Task
+    {
+        static void Main()
+        {
+            char[] str = Console.ReadLine().ToCharArray();
+            int vowelsCount = GetVowelsCount(str);
+
+            Console.WriteLine(vowelsCount);
+
+        }
+
+        static int GetVowelsCount(char[] arr)
+        {
+            List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+            int vowelsCount = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (vowels.Contains(char.ToLower(arr[i])))
+                {
+                    vowelsCount++;
+                }
+            }
+
+            return vowelsCount;
+        }
+    }
+}
