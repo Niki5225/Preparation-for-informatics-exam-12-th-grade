e827f583bf3b0571cf9cf38d8c27f89f6617ae50
Niki5225 <122698681+Niki5225@users.noreply.github.com>
2024-05-09 12:12:33 +0300

    sets and dicts

---
+++ sets and dicts advanced/CountSymbols.cs
@@ -0,0 +1,28 @@
+﻿namespace CountSymbols
+{
+    class Task
+    {
+        static void Main()
+        {
+            SortedDictionary<char, int> dict = new SortedDictionary<char, int>();
+            string input = Console.ReadLine();
+
+            foreach(char ch in input)
+            {
+                if (dict.ContainsKey(ch))
+                {
+                    dict[ch]++;
+                }
+                else
+                {
+                    dict.Add(ch, 1);
+                }
+            }
+
+            foreach(var item in dict)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
---
+++ sets and dicts advanced/Wardrobe.cs
@@ -0,0 +1,54 @@
+﻿namespace Wardrobe
+{
+    class Task
+    {
+        static void Main()
+        {
+            int numberOfClothes = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, int>> clothesDict = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < numberOfClothes; i++)
+            {
+                string[] data = Console.ReadLine().Split(" -> ");
+                string color = data[0];
+                string[] clothes = data[1].Split(",");
+
+                if (!clothesDict.ContainsKey(color))
+                {
+                    clothesDict[color] = new Dictionary<string, int>();
+                }
+
+                foreach (string s in clothes)
+                {
+                    if (clothesDict[color].ContainsKey(s))
+                    {
+                        clothesDict[color][s]++;
+                    } else
+                    {
+                        clothesDict[color][s] = 1;
+                    }
+                }
+            }
+
+            string[] searchedInfo = Console.ReadLine().Split();
+            string searchedColor = searchedInfo[0];
+            string searchedDress = searchedInfo[1];
+
+
+            foreach(string color in clothesDict.Keys)
+            {
+                Console.WriteLine($"{color} clothes:");
+                foreach(var item in clothesDict[color])
+                {
+                    if (searchedColor == color && searchedDress == item.Key)
+                    {
+                        Console.WriteLine($"* {item.Key} - {item.Value} (found!)");
+                    } else
+                    {
+                        Console.WriteLine($"* {item.Key} - {item.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
